11bfb3372aaa4a5ea96abc5e9731b30eb7b4084a
rahulsub9 <27subramaniamr@smtexas.org>
2025-05-13 16:51:45 -0500

    Create FinancialAdvisor.java

---
+++ FinancialAdvisor.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class FinancialAdvisor
+{
+    //Constants for financial thresholds
+    //Used 15.0 for more achievable target
+    private static final double ADEQUATE_SAVINGS_RATE_PERCENT = 15.0;
+    private static final double HIGH_DEBT_RATIO_PERCENT = 36.0;
+}
